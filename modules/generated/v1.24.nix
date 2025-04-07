e19dec65285d1d01d54e44960f9ac4ad40fb2d05
Diamondy4 <diamondy4@hotmail.com>
2025-04-07 20:07:40 +0300

    fix: workaround for hall/kubenix#52

--- modules/generated/v1.24.nix
+++ modules/generated/v1.24.nix
@@ -5719,7 +5719,7 @@ let
         };
         "ports" = mkOption {
           description = "List of ports to expose from the container. Exposing a port here gives the system additional information about the network connections a container uses, but is primarily informational. Not specifying a port here DOES NOT prevent that port from being exposed. Any port which is listening on the default \"0.0.0.0\" address inside a container will be accessible from the network. Cannot be updated.";
-          type = (types.nullOr (coerceAttrsOfSubmodulesToListByKey "io.k8s.api.core.v1.ContainerPort" "name" [ "containerPort" "protocol" ]));
+          type = (types.nullOr (coerceAttrsOfSubmodulesToListByKey "io.k8s.api.core.v1.ContainerPort" "name" [ "containerPort" ]));
           apply = attrsToList;
         };
         "readinessProbe" = mkOption {
@@ -6369,7 +6369,7 @@ let
         };
         "ports" = mkOption {
           description = "Ports are not allowed for ephemeral containers.";
-          type = (types.nullOr (coerceAttrsOfSubmodulesToListByKey "io.k8s.api.core.v1.ContainerPort" "name" [ "containerPort" "protocol" ]));
+          type = (types.nullOr (coerceAttrsOfSubmodulesToListByKey "io.k8s.api.core.v1.ContainerPort" "name" [ "containerPort" ]));
           apply = attrsToList;
         };
         "readinessProbe" = mkOption {
